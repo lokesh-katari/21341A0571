e5c2c296918fd58da289005b101c55e1e3dedb2d
lokesh-katari <lokeshkatari921@outlook.com>
2024-08-15 13:21:27 +0530

    added frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,16 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [product, setProduct] = useState([]);
+  const [product, setProduct] = useState<
+    { productName: string; price: string; rating: string }[]
+  >([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
       const { data } = await axios.get(
         "http://localhost:3000/products?company=AMZ&category=Phone&top=10&minPrice=100&maxPrice=100000"
       );
-      setProduct(data);
+      setProduct(data.products);
     };
     fetchProducts();
   }, []);
@@ -21,20 +23,24 @@ function App() {
         Hello world! these are Products
       </h1>
 
-      {product.map((item, index) => (
-        <a
-          href="#"
-          className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-        >
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-        </a>
-      ))}
+      <div className="flex  items-center just gap-5">
+        {product.map((item, index) => (
+          <a
+            href="#"
+            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {item.productName}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {item.price}
+            </p>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {item.rating}
+            </p>
+          </a>
+        ))}
+      </div>
     </>
   );
 }
